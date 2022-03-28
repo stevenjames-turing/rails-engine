f26a4ac141fd5e40d6a48cd2d0ae7f533d170137
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-28 16:40:44 -0500

    Test: Merchant has many items

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do 
     it { should validate_presence_of :name }
   end
-  
+  describe 'relationships' do 
+    it { should have_many :items }
+  end
 end
