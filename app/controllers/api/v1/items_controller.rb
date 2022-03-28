05c69070199c118421148d34466c87c089c40681
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-28 17:12:32 -0500

    Feat: API can send a list of all items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::ItemsController < ApplicationController
-  before_action :find_merchant, only: [:index]
+  # before_action :find_merchant, only: [:index]
 
   def index
-    render json: @merchant.items 
+    if params[:merchant_id]
+      find_merchant
+      render json: @merchant.items
+    else 
+      render json: Item.all
+    end
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do 
         resources :items, only: [:index]
       end
+      resources :items, only: [:index]
     end
   end
 end
