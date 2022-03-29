1d378d970081e71c709a2a0837fcb9a969f59e67
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-29 14:42:23 -0500

    Refactor: MerchantItemsController updated to use Serializer

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,17 +1,24 @@
 class Api::V1::MerchantItemsController < ApplicationController
-  before_action :find_merchant, only: [:index]
 
-  def index 
-    json_response(ItemSerializer.new(@merchant.items))
+  def index
+    if params[:item_id]
+      find_merchant_and_item
+      json_response(MerchantSerializer.new(@merchant))
+    elsif params[:merchant_id]
+      find_merchant
+      json_response(ItemSerializer.new(@merchant.items))
+    end
   end
 
   private 
 
+    
     def find_merchant
       @merchant = Merchant.find(params[:merchant_id])
     end
 
-    def find_item
+    def find_merchant_and_item
       @item = Item.find(params[:item_id])
+      @merchant = Merchant.find(@item.merchant_id)
     end
 end
