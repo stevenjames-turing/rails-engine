c385035606b6a48a18ed3afa724e0eb9f2f7bfa8
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-29 16:49:45 -0500

    Feat: API can delete invoice if all items have been destroyed

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,19 @@ class Item < ApplicationRecord
                         :merchant_id
 
   # Model Relationships
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
   belongs_to :merchant
-  
+
+  def valid_invoice?
+    ii = InvoiceItem.where(item_id: id)
+    if ii.count == 1 
+      i = Invoice.where(id: ii[0].id)
+      if i[0].invoice_items.count == 1
+        {id: i[0].id}
+      else 
+        nil 
+      end
+    end
+  end
 end
