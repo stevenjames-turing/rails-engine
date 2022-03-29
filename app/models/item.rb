f32ef249f2f6858f47ddc0bb07ff4707fc28b825
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-28 20:25:13 -0500

    Refactor: Update Item model for new relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,14 @@
 class Item < ApplicationRecord
 
   # Model Validations
-  validates_presence_of :name, :description, :unit_price
+  validates_presence_of :name,
+                        :description,
+                        :unit_price,
+                        :merchant_id
 
   # Model Relationships
-  belongs_to :merchant 
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
+  belongs_to :merchant
   
 end
