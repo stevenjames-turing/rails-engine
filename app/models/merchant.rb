7bd289f4ebbf67bf83ea7540bcda36cf26593f28
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-28 20:23:14 -0500

    Refactor: Update Merchant model with new model relationships

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,13 @@
 class Merchant < ApplicationRecord
+
   # Model Validations
   validates_presence_of :name
   
   # Model Relationships
   has_many :items
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
     
 end
