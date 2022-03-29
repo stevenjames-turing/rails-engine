b227a5ea238049be33030ee41b3d3b8fba828bfd
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-29 17:29:03 -0500

    Update endpoints to include non-restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/api/vi/items/find', to: 'items#find'
+  get '/api/vi/items/find_all', to: 'items#find_all'
+  
+  get '/api/vi/merchants/find', to: 'merchants#find'
+  get '/api/vi/merchants/find_all', to: 'merchants#find_all'
+
   namespace :api do 
     namespace :v1 do 
       resources :merchants, only: [:index, :show] do 
