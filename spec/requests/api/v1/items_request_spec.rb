2a336dea49736ec7cac0777f583fbe8f640edcfd
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-28 20:56:56 -0500

    Test: Item#index returns an array of data when only 1 resource found

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,33 +1,72 @@
 require 'rails_helper'
 
 describe "Items API" do
-  it 'sends a list of items' do
-    create_list(:item, 3)
+  context 'Item#index' do
+    context 'happy_path' do 
+      it 'sends a list of items' do
+        create_list(:item, 3)
+    
+        get '/api/v1/items'
+    
+        expect(response).to be_successful
+    
+        items = JSON.parse(response.body, symbolize_names: true)
+        expect(items).to be_an Array
+    
+        expect(items.count).to eq(3)
+    
+        items.each do |item|  
+          expect(item).to have_key(:id)
+          expect(item[:id]).to be_an Integer
+    
+          expect(item).to have_key(:merchant_id)
+          expect(item[:merchant_id]).to be_an Integer
+    
+          expect(item).to have_key(:name)
+          expect(item[:name]).to be_a String
+    
+          expect(item).to have_key(:description)
+          expect(item[:description]).to be_a String
+    
+          expect(item).to have_key(:unit_price)
+          expect(item[:unit_price]).to be_a Float
+        end 
+      end
+      context 'sad_path' do 
+        it 'returns an array of data even if only 1 resource is found' do 
+          create(:item)
+      
+          get '/api/v1/items'
+      
+          expect(response).to be_successful
+      
+          items = JSON.parse(response.body, symbolize_names: true)
+          expect(items).to be_an Array
+      
+          expect(items.count).to eq(1)
+      
+          items.each do |item|  
+            expect(item).to have_key(:id)
+            expect(item[:id]).to be_an Integer
+      
+            expect(item).to have_key(:merchant_id)
+            expect(item[:merchant_id]).to be_an Integer
+      
+            expect(item).to have_key(:name)
+            expect(item[:name]).to be_a String
+      
+            expect(item).to have_key(:description)
+            expect(item[:description]).to be_a String
+      
+            expect(item).to have_key(:unit_price)
+            expect(item[:unit_price]).to be_a Float
+          end 
+        end
+        it 'returns an array of data even if no resources are found' do 
 
-    get '/api/v1/items'
-
-    expect(response).to be_successful
-
-    items = JSON.parse(response.body, symbolize_names: true)
-
-    expect(items.count).to eq(3)
-
-    items.each do |item|  
-      expect(item).to have_key(:id)
-      expect(item[:id]).to be_an Integer
-
-      expect(item).to have_key(:merchant_id)
-      expect(item[:merchant_id]).to be_an Integer
-
-      expect(item).to have_key(:name)
-      expect(item[:name]).to be_a String
-
-      expect(item).to have_key(:description)
-      expect(item[:description]).to be_a String
-
-      expect(item).to have_key(:unit_price)
-      expect(item[:unit_price]).to be_a Float
-    end 
+        end
+      end
+    end
   end
 
   it 'can get one item by its ID' do 
