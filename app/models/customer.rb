1cd1493f96a7d57742b45554acea89fc71d64e9b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-28 20:29:13 -0500

    Feat: Customer validations and relationships complete

---
+++ app/models/customer.rb
@@ -0,0 +1,11 @@
+class Customer < ApplicationRecord
+
+  # Model Validations
+  validates_presence_of :first_name,
+                        :last_name
+  
+  # Model Relationships
+  has_many :invoices
+  has_many :merchants, through: :invoices
+  has_many :transactions, through: :invoices
+end
