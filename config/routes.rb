278830585adda325a81e5d421608a9825043ca4a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-29 20:37:27 -0500

    Refactor: update non-restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/api/vi/items/find', to: 'items#find'
-  get '/api/vi/items/find_all', to: 'items#find_all'
   
-  get '/api/vi/merchants/find', to: 'merchants#find'
-  get '/api/vi/merchants/find_all', to: 'merchants#find_all'
-
+  
   namespace :api do 
     namespace :v1 do 
+      get 'merchants/find', to: 'merchants#find'
+      get 'merchants/find_all', to: 'merchants#find_all'
       resources :merchants, only: [:index, :show] do 
         resources :items, controller: 'merchant_items', only: [:index]
       end
+      get 'items/find', to: 'items#find'
+      get 'items/find_all', to: 'items#find_all'
       resources :items, only: [:index, :show, :create, :update, :destroy] do 
         resources :merchant, controller: 'merchant_items', only: [:index]
       end
