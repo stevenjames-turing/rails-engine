ea8e7f66a3a172193b60f4ff5e6400610c166e8e
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-30 21:42:22 -0500

    Test: item#find_all will return an error if max price param missing

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -882,6 +882,19 @@ describe 'Items API' do
 
           expect(response).to_not have_http_status(404)
         end
+        
+        it 'should return an error if max price param is empty' do 
+          item1 = create(:item, unit_price: 3.25, name: 'DEF Item')
+          item2 = create(:item, unit_price: 8.25, name: 'CDE Item')
+          item3 = create(:item, unit_price: 11.28, name: 'BCD Item')
+          item4 = create(:item, unit_price: 20, name: 'ABC Item')
+
+          get '/api/v1/items/find_all?max_price='
+
+          items = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response).to_not have_http_status(404)
+        end
       end
 
       context 'min and max_price' do
