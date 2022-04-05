c0b6d30aa8abdc4bec0fa0bfb20ae0d128980d49
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-05 15:34:48 -0500

    Feat: merchant can now find object with most revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -19,16 +19,24 @@ class Merchant < ApplicationRecord
       .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) as total_revenue")
       .order('total_revenue DESC')
       .limit(number)
-    end
-    
-    def self.top_merchants_by_items_sold(number)
-      Merchant
-        .joins(invoices: [:invoice_items, :transactions])
-        .where(transactions: { result: 'success' }, invoices: { status: 'shipped' })
-        .group('merchants.id')
-        .select("merchants.*, SUM(invoice_items.quantity) as items_sold")
-        .order('items_sold DESC')
-        .limit(number)
+  end
+  
+  def self.top_merchants_by_items_sold(number)
+    Merchant
+      .joins(invoices: [:invoice_items, :transactions])
+      .where(transactions: { result: 'success' }, invoices: { status: 'shipped' })
+      .group('merchants.id')
+      .select("merchants.*, SUM(invoice_items.quantity) as items_sold")
+      .order('items_sold DESC')
+      .limit(number)
+  end
+
+  def self.total_revenue(merchant_id)
+    Merchant
+    .joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: { result: 'success' }, invoices: { status: 'shipped' }, merchants: {id: merchant_id})
+    .group('merchants.id')
+    .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) as total_revenue")
   end
 
 end
