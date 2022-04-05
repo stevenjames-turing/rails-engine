995322cdaa5517844941a2b80232d5b751482e0c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-05 12:45:43 -0500

    Feat: return top merchants by revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,4 +11,14 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 
+  def self.top_merchants_by_revenue(number)
+    Merchant
+      .joins(invoices: [:invoice_items, :transactions])
+      .where(transactions: { result: 'success' }, invoices: { status: 'shipped' })
+      .group('merchants.id')
+      .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) as total_revenue")
+      .order('total_revenue DESC')
+      .limit(number)
+  end
+
 end
