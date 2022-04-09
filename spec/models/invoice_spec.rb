28f42dd90dc9a2afb357736ccc1845190a09d115
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-04-09 15:19:19 -0600

    Refactor: Update model relationships

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,8 +8,9 @@ RSpec.describe Invoice, type: :model do
   end
   describe 'relationships' do
     it { should belong_to :customer }
-    it { should have_many(:items).through(:invoice_items) }
-    it { should have_many(:merchants).through(:items) }
+    it { should belong_to :merchant }
     it { should have_many :transactions }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items) }
   end
 end
